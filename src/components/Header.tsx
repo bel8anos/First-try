4a261b39be1c253fa61f5494c7ee9e0319d326ed
Th.Bellas <bel8anos@gmail.com>
2025-10-20 14:04:31 +0300

    use svg logo and fix Next/Image warning (unoptimized)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,11 +24,12 @@ export default function Header() {
           <div className="flex-shrink-0">
             <Link href="/" className="flex items-center">
               <Image
-                src="/ftd-logo.png"
+                src="/ftd-logo.svg"
                 alt="First Try Distribution"
                 width={120}
                 height={40}
                 className="h-10 w-auto"
+                unoptimized
               />
             </Link>
           </div>
